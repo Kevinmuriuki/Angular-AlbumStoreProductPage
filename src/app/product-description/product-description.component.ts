cfc9266049469009d4c996a73758087a595003bf
Kevin Muriuki <kevinmuriukiwaweru@gmail.com>
2020-09-28 23:07:31 +0300

    corrected all the erros

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,21 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { inject } from '@angular/core/src/render3';
 
-import '../product.service';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
   styleUrls: ['./product-description.component.css']
 })
+
 export class ProductDescriptionComponent implements
 OnInit {
 
   albumInfo;
 
-  constructor(private _productService) { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(responce => this.albumInfo = response);
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http,Response } from '@angular/http';
-
-import `rxjs/add/operator/map`;
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -10,8 +9,6 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
-   return this._http.get(this._albumUrl).response.json();
-
+   return this._http.get(this._albumUrl).map(response => this._albumUrl = response.json());
   }
-
 }
