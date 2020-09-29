7ab1e90038bf4b7f7f6a1787f4ddf5c0c24735bc
Kevin Muriuki <kevinmuriukiwaweru@gmail.com>
2020-09-29 19:55:35 +0300

    modified the product service by  adding a method and a property

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,15 +2,21 @@ import { Injectable } from '@angular/core';
 import { Http,Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
+import { Product } from './product';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
    return this._http.get(this._albumUrl).map(response => this._albumUrl = response.json<Album>());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(_productsUrl).map(response => this._productsUrl = response.json<Product[]>());
   }
 }
