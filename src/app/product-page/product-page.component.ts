86ca089d4ccc1e3b08932a8f3807a83abadfd35c
Kevin Muriuki <kevinmuriukiwaweru@gmail.com>
2020-09-29 18:37:38 +0300

    modified product page to its original state corrected the product tracklisting

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Album } from '../album';
-import { ProductService } from '../product.service';
-
 
 @Component({
   selector: 'app-product-page',
@@ -10,12 +7,9 @@ import { ProductService } from '../product.service';
 })
 export class ProductPageComponent implements OnInit {
 
-  albumInfo: Album;
-
-  constructor(private _productService) { }
+  constructor() { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 
 }
