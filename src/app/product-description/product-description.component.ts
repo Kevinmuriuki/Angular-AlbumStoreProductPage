1b926061f8d234303f9e8ed298fcd34138814106
Kevin Muriuki <kevinmuriukiwaweru@gmail.com>
2020-09-29 17:24:49 +0300

    added a type to the album info property in product description

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -13,7 +13,7 @@ import { Album } from '../album';
 export class ProductDescriptionComponent implements
 OnInit {
 
-  albumInfo;
+  albumInfo: Album;
 
   constructor(private _productService: ProductService) { }
 
